36bc76ed71afc029945991e658f4bb6395649218
kp22FitkalDmytro <117391309+kp22FitkalDmytro@users.noreply.github.com>
2022-11-11 00:28:57 +0200

    Add files via upload

---
+++ sem_1_lab_1/0.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACD
+{
+    internal class Program
+    {
+        public static float x=0;
+        public static float a=0;
+        public static float b=0;
+        public static float y = 0;
+        public static float temp = 0;
+        public static string console_temp="";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
