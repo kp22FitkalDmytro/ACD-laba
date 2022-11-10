299e6bbc715f724cd306c195c8f9468e5472d070
kp22FitkalDmytro <117391309+kp22FitkalDmytro@users.noreply.github.com>
2022-11-11 00:29:31 +0200

    Тестовий сценарій

---
+++ sem_1_lab_1/0_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_2
+{
+    internal class Program
+    {
+        public static List<char> user_input; 
+        public static List<char> user_input_with_square;  
+
+        public static bool is_poly ;
+        public static int symbol_count=0;
+        public static bool is_count_has_pair;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(symbol_count);
+        }
+    }
+}
