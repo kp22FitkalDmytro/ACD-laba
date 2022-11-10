ee9622543e797fd15e54d0e028b5d97122f850df
kp22FitkalDmytro <117391309+kp22FitkalDmytro@users.noreply.github.com>
2022-11-11 01:25:59 +0200

    Перше завдання

---
+++ sem_1_lab_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class lab2
+    {
+        public static float x;
+        public static float a;
+        public static float b;
+        public static float y;
+        public static float temp;
+        public static string console_temp;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть y");
+            console_temp = Console.ReadLine();
+            float.TryParse(console_temp, out y);
+            Console.WriteLine("");
+            Console.WriteLine("Введiть x");
+            console_temp = Console.ReadLine();
+            float.TryParse(console_temp, out x);
+            temp = (float)Math.Cos(y);
+            temp = (float)Math.Sin(x) - temp;
+            temp = (float)Math.Abs(temp);
+            temp = (float)(Math.Pow(temp, (float)1 / 3));
+            a = temp;
+            Console.WriteLine("a = " + a.ToString());
+            temp = a * a;
+            temp = (float)Math.Sin(temp);
+            temp = (float)Math.Cos(temp);
+            b = temp;
+            Console.WriteLine("b = "+ b.ToString());
+        }
+    }
+}
